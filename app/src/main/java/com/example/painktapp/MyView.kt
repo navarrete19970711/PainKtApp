be8ad3653f886ca08811f39fc016b2b2de9697b9
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-08-28 11:39:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/painktapp/MyView.kt
@@ -0,0 +1,12 @@
+package com.example.painktapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+// 1) viweを継承したクラス
+class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+   
+}
