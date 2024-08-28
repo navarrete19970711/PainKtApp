902d996006ef587fa1392f0d7f821d7d494b9e65
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-08-28 11:43:10 +0900

    これが描画の準備

--- app/src/main/java/com/example/painktapp/MyView.kt
+++ app/src/main/java/com/example/painktapp/MyView.kt
@@ -8,6 +8,9 @@ import android.view.View
 // 1) viweを継承したクラス
 class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    
+    // 2) 描画の準備
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
 
 }
